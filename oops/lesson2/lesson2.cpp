0c71269143e412c340b899e58ba0de4e9cac8865
manxiansheng666 <1131620594@qq.com>
2022-08-17 17:35:31 +0800

    三种设计模式

---
+++ oops/lesson2/lesson2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Operation
+{
+public:
+    virtual int operate(int,int) = 0;
+    virtual ~Operation() {};
+};
+
+
+//策略模式 strategy
+class Calculator
+{
+public:
+    void run(Operation* op)
+    {
+        cout << op->operate(a,b) << endl;
+        delete op;
+    }
+
+private:
+    int a = 10;
+    int b = 20;
+};
+
+class AddOperation :public Operation
+{
+private:
+    int operate(int a, int b) override
+    {
+        return a + b;
+    }
+    ~AddOperation()
+    {
+        cout << __FUNCTION__ << endl;
+    }
+};
+
+class SubOperation :public Operation
+{
+private:
+    int operate(int a, int b) override
+    {
+        return a - b;
+    }
+    ~SubOperation()
+    {
+        cout << __FUNCTION__ << endl;
+    }
+};
+
+
+int main()
+{
+    Calculator c;
+    c.run(new AddOperation);
+    c.run(new SubOperation);
+}
+
---
+++ oops/lesson3/lesson3.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+//观察者模式
+class Iobsersver
+{
+public:
+    virtual void obsersve(int i,int num) = 0;
+    virtual ~Iobsersver() {};
+};
+
+
+class Person
+{
+public:
+    Person() = default;
+    Person(int num) :num(num) {};
+
+    void run()
+    {
+        for (int i = 0; i < num; i++)
+        {
+//            cout << "running!\r\n" << endl;
+            observer(i,num);
+        }
+    }
+
+    void add_ob(Iobsersver* ob)
+    {
+        observers.push_back(ob);
+    }
+
+    void remove_ob(Iobsersver* ob)
+    {
+        observers.remove(ob);
+    }
+
+private:
+    int num = 0;
+    list<Iobsersver*> observers;
+    void observer(int i,int num)
+    {
+        for (const auto& ob : observers)
+        {
+            ob->obsersve(i,num);
+        }
+    }
+};
+
+class Student
+{
+public:
+    void print_age()
+    {
+        cout << age << endl;
+    }
+private:
+    int age = 10;
+};
+
+class ob1 :public Student, public Iobsersver
+{
+private:
+
+    virtual void obsersve(int i, int num) override
+    {
+        cout << '.';
+    }
+};
+
+class ob2 :public Student, public Iobsersver
+{
+private:
+
+    virtual void obsersve(int i, int num) override
+    {
+        cout << '+';
+    }
+};
+
+class ob3 :public Student, public Iobsersver
+{
+private:
+
+    virtual void obsersve(int i, int num) override
+    {
+        cout << i << '*';
+    }
+};
+
+int main()
+{
+    Person p(10);
+    ob1 o1;
+    ob2 o2;
+    ob3 o3;
+    o1.print_age();
+    p.add_ob(&o1);
+    p.add_ob(&o2);
+    p.add_ob(&o3);
+    p.remove_ob(&o2);
+    p.run();
+}
