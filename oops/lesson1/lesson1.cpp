ba8ecbb9409514c7d2df8eb5a46ce755a3783b96
manxiansheng666 <1131620594@qq.com>
2022-08-17 15:42:45 +0800

    template设计模式

---
+++ oops/lesson1/lesson1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//template
+class Person
+{
+public:
+    void run()
+    {
+        step1();
+        step2();
+        step3();
+    }
+    virtual ~Person() {}
+
+private:
+    void step1()
+    {
+        cout << __FUNCTION__ << endl;
+    }
+
+protected:
+    virtual void step2() = 0;
+    virtual void step3() = 0;
+};
+
+
+class Student :public Person
+{
+private:
+    void step2() override
+    {
+        cout << __FUNCTION__ << endl;
+    }
+
+    void step3() override
+    {
+        cout << __FUNCTION__ << endl;
+    }
+};
+
+
+int main()
+{
+    Person* p = new Student;
+    p->run();
+}
