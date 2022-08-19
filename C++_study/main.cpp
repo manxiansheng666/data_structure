7b7747161f4ac19308964e59d8f098b04dd398cc
manxiansheng666 <1131620594@qq.com>
2022-08-19 19:04:25 +0800

    桥模式

--- C++_study/main.cpp
+++ C++_study/main.cpp
@@ -2,17 +2,25 @@
 #include <string>
 using namespace std;
 
+class Person
+{
+	
+public:
+	Person(int age) :age(age) {}
+	int age;
+};
+
+
+class Student :public Person
+{
+public:
+	using Person :: Person;
+};
 
 int main()
 {
-	for (int i = 1; i <= 9; i++)
-	{
-		for (int j = 9; j >= i; j--)
-		{
-			cout << i << 'x' << j << '=' << i * j << "\t";
-		}
-		cout << endl;
-	}
-		
+	Student s(10);
+
+
 	return 0;
 }
---
+++ oops/lesson5/lesson5.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Messagehd
+{
+public:
+    virtual void playsound() = 0;
+};
+
+class Message
+{
+protected:
+    Messagehd* mhd;
+public:
+    Message(Messagehd* mhd) :mhd(mhd) {}
+    virtual void login() = 0;
+};
+
+class Pcmhd :public Messagehd
+{
+private:
+    void playsound() override
+    {
+        cout << "pc hardware" << endl;
+    }
+};
+
+class Moblehd :public Messagehd
+{
+private:
+    void playsound() override
+    {
+        cout << "moble hardware" << endl;
+    }
+};
+
+class Lite :public Message
+{
+public:
+    using Message::Message;
+    void login() override
+    {
+        cout << "lite login" << endl;
+        mhd->playsound();
+    }
+};
+
+class Prefect :public Message
+{
+public:
+    using Message::Message;
+    void login() override
+    {
+        cout << "Prefect login" << endl;
+        mhd->playsound();
+    }
+};
+
+int main()
+{
+    Messagehd* mhd = new Pcmhd;
+    Lite* lpc = new Lite(mhd);
+    lpc->login();
+    Prefect* ppc = new Prefect(mhd);
+    ppc->login();
+    return 0;
+}
